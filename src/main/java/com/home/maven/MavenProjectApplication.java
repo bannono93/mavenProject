01ee9e40edb6de4303b92bc4d2532db7d4bb9c95
banno <banno@bannono93>
2022-09-12 18:58:53 +0900

    df

---
+++ src/main/java/com/home/maven/MavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.home.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenProjectApplication.class, args);
+	}
+
+}
